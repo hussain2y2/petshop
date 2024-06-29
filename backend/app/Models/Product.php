bdc778e821f14534a67374304f5310988bca8f52
Hussain Ahmad <engrhussainahmad@gmail.com>
2024-06-29 06:45:26 +0200

    Updating Category and Product models for foreign key as category_uuid

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -38,6 +38,6 @@ class Product extends Model
 
     public function category(): BelongsTo
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class, 'category_uuid', 'uuid');
     }
 }
