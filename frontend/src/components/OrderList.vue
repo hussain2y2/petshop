a573b83b05c06a9c194d5ee60d35ddfd2c8ba080
Hussain Ahmad <engrhussainahmad@gmail.com>
2024-06-30 14:15:48 +0200

    Implementing OrderList logic

--- frontend/src/components/OrderList.vue
+++ frontend/src/components/OrderList.vue
@@ -1,10 +1,34 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-
+  <v-data-table :items="orders" :headers="headers">
+    <template v-slot:[`item.action`]="{ item }">
+      <v-btn @click="viewOrder(item.uuid)">View</v-btn>
+    </template>
+  </v-data-table>
 </template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useOrderStore } from '@/store';
+import { useRouter } from 'vue-router';
+
+const orderStore = useOrderStore();
+const router = useRouter();
+const orders = orderStore.orders;
+
+const headers = [
+  { text: 'Order ID', value: 'uuid' },
+  { text: 'Amount', value: 'amount' },
+  { text: 'Action', value: 'action' },
+];
+
+const viewOrder = (id: string) => {
+  router.push({ name: 'order', params: { id } });
+};
+
+onMounted(() => {
+  orderStore.fetchOrders();
+});
+</script>
 
 <style scoped>
 
