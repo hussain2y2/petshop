4238f78991857a52fc5168a1904839e3401f862b
Hussain Ahmad <engrhussainahmad@gmail.com>
2024-06-30 14:10:54 +0200

    Defining DefaultLayout

--- frontend/src/layouts/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout.vue
@@ -1,11 +1,19 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>Pet Shop</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style scoped>
+<script setup lang="ts">
+import { useAuthStore } from '@/stores';
+const authStore = useAuthStore();
+</script>
 
+<style scoped>
+@import '~vuetify/styles';
 </style>
